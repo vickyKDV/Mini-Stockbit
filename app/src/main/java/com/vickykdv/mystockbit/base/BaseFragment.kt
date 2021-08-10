354b9f4ebd863d5e6fdf3b037e70cca5ae80318f
vicky KDV <vickykdv@gmail.com>
2021-08-10 23:28:10 +0700

    Remove BaseActivity as Parent

--- app/src/main/java/com/vickykdv/mystockbit/base/BaseFragment.kt
+++ app/src/main/java/com/vickykdv/mystockbit/base/BaseFragment.kt
@@ -9,21 +9,6 @@ import androidx.fragment.app.Fragment
 
 abstract class BaseFragment:Fragment() {
 
-    private var mContext: Context? = null
-    private lateinit var mBaseActivity: BaseActivity
-    private var mInflater: LayoutInflater? = null
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mContext = activity
-        if (context is BaseActivity) {
-            mBaseActivity = mContext as BaseActivity
-            mInflater = LayoutInflater.from(mBaseActivity)
-        } else {
-            throw ClassCastException("The activity is not child of BaseActivity")
-        }
-    }
 
     abstract fun setLayout(): View
 
@@ -36,4 +21,8 @@ abstract class BaseFragment:Fragment() {
         return setLayout()
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onCreateActivity(savedInstanceState)
+    }
 }
